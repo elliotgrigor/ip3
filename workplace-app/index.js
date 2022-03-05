3802ac17db9ee6ba6df1a4a6519b01cb2f21c5ad
elliotgrigor <egrigo200@caledonian.ac.uk>
2022-03-05 12:49:58 +0000

    added root route and view engine

--- workplace-app/index.js
+++ workplace-app/index.js
@@ -1,2 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log(`Workplace app running on http://${process.env.IP}:${process.env.PORT}`);
+});
