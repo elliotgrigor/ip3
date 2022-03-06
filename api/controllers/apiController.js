ecabd302d7085dedde94906ca3119fb470c9da22
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-06 15:57:28 +0000

    moved database connection to controller module

--- api/controllers/apiController.js
+++ api/controllers/apiController.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.3kfaf.azure.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true },
+  );
+}
 
 exports.getAllEmployees = (req, res) => {
   res.json({ data: "employee_data" });
--- api/index.js
+++ api/index.js
@@ -2,18 +2,8 @@ require('dotenv').config()
 
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 
 const routes = require('./routes/apiRoutes');
-
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.3kfaf.azure.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true },
-  );
-}
 
 app.use('/api/v1', routes);
 
