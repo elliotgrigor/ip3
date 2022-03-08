24f393b59b2ec716867c36ea1bf76cfb98f864d7
Innex42 <innex4@gmail.com>
2022-03-08 20:52:09 +0000

    Added Routers for index cards

--- workplace-app/routes/pageRoutes.js
+++ workplace-app/routes/pageRoutes.js
@@ -6,5 +6,11 @@ const controller = require('../controllers/pageController');
 router.get('/', controller.home);
 router.get('/login', controller.login);
 router.post('/login', controller.login);
+router.get('/payslips', controller.payslips); 
+router.get('/rota', controller.rota);
+//router.post('/rota', controller.rota);
+router.get('/myDetails', controller.myDetails);
+router.get('/punchIn', controller.punchIn);
+router.get('/employee', controller.employee);
 
 module.exports = router;
