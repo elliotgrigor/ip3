782e7b9fd9c9f194f5c8ec0e2ea71065becc64ee
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-08 22:39:40 +0000

    added password encryption to seeded data

--- api/seed.js
+++ api/seed.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 main().catch(err => console.log(err));
 
@@ -13,48 +14,52 @@ async function main() {
 
 const Employee = require('./models/employeeModel');
 
-const john = new Employee({
-  staffNumber: 'A001',
-  firstName: 'John',
-  lastName: 'Smith',
-  DOB: new Date(1997, 2, 23),
-  gender: 'M',
-  profilePic: 'http://something.net',
-  access: {
-    level: 2,
-  },
-  password: 'very secure password',
-  contact: {
-    address: {
-      houseNumber: 20,
-      street: 'Sesame Street',
-      postCode: 'AB1 2CD',
-      city: 'New York City',
-    },
-    phone: '+441234567890',
-    email: 'john.smith@email.com',
-  },
-  daysWorked: [
-    {
-      forDate: new Date().toDateString(),
-      startTime: new Date(2022, 1, 28, 8, 47, 0, 0),
-      finishTime: new Date(2022, 1, 28, 17, 2, 0, 0),
-      dailyHours: 8,
-    },
-  ],
-  payRate: 100.81,
-  natInsNumber: 'AA123456B',
-  payslips: [
-    {
-      grossPay: 1200.00,
-      netPay: 1050.00,
-      natInsContrib: 50.00,
-      incomeTax: 50.00,
-      taxCode: 'S1250',
-      pensionContrib: 50.00,
-      issueDate: new Date(),
-    },
-  ],
-});
+bcrypt.genSalt(10, function(err, salt) {
+  bcrypt.hash('nonce', salt, function(err, hash) {
+    const john = new Employee({
+      staffNumber: 'A001',
+      firstName: 'John',
+      lastName: 'Smith',
+      DOB: new Date(1997, 2, 23),
+      gender: 'M',
+      profilePic: 'http://something.net',
+      access: {
+        level: 2,
+      },
+      password: hash,
+      contact: {
+        address: {
+          houseNumber: 20,
+          street: 'Sesame Street',
+          postCode: 'AB1 2CD',
+          city: 'New York City',
+        },
+        phone: '+441234567890',
+        email: 'john.smith@email.com',
+      },
+      daysWorked: [
+        {
+          forDate: new Date().toDateString(),
+          startTime: new Date(2022, 1, 28, 8, 47, 0, 0),
+          finishTime: new Date(2022, 1, 28, 17, 2, 0, 0),
+          dailyHours: 8,
+        },
+      ],
+      payRate: 100.81,
+      natInsNumber: 'AA123456B',
+      payslips: [
+        {
+          grossPay: 1200.00,
+          netPay: 1050.00,
+          natInsContrib: 50.00,
+          incomeTax: 50.00,
+          taxCode: 'S1250',
+          pensionContrib: 50.00,
+          issueDate: new Date(),
+        },
+      ],
+    });
 
-john.save();
+    john.save();
+  });
+});
