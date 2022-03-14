737253d5945bb5f5d845c49edfa3a92c54c5859c
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-14 17:58:27 +0000

    changed session database location

--- workplace-app/app.js
+++ workplace-app/app.js
@@ -17,7 +17,7 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
-  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' }),
+  store: new SQLiteStore({ db: 'sessions.db', dir: '../db' }),
 }));
 app.use(passport.authenticate('session'));
 
