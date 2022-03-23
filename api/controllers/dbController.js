3c2a8d6aea2aa8c98357078536776e3e4414b286
elliotgrigor <elliot.grigor@protonmail.com>
2022-03-23 19:38:28 +0000

    updated database loading and exporting

--- api/controllers/dbController.js
+++ api/controllers/dbController.js
@@ -1,17 +1,11 @@
 const Datastore = require('@seald-io/nedb');
 
-const employees = new Datastore({ filename: '../db/employees.collection' });
-const rotas = new Datastore({ filename: '../db/rotas.collection' });
+exports.employees = new Datastore({
+  filename: '../db/employees.collection',
+  autoload: true,
+});
 
-exports.employees = employees;
-exports.rotas = rotas;
-
-exports.load = () => {
-  employees.loadDatabase(err => {
-    if (err) return console.log(err)
-  });
-
-  rotas.loadDatabase(err => {
-    if (err) return console.log(err)
-  });
-}
+exports.rotas = new Datastore({
+  filename: '../db/rotas.collection',
+  autoload: true,
+});
