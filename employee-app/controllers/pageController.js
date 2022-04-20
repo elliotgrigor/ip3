e6a27db17e493eda76146cb1b64cfb47df0ac0a3
Devlin <drcortens@gmail.com>
2022-04-20 16:23:03 +0100

    Added editing of logged in user
    Added editing of logged in user

--- employee-app/controllers/pageController.js
+++ employee-app/controllers/pageController.js
@@ -4,11 +4,35 @@ exports.home = (req, res) => {
 };
 
 exports.profile = (req, res) => {
-  res.render('staffProfile', {});
+  res.render('staffProfile', {user: req.user});
 }
 
 exports.editProfile = (req, res) => {
-  res.render('editProfile', {});
+  if(req.method === 'GET') {
+    res.render('editProfile', {user: req.user});
+  }
+  else if(req.method === 'POST') {
+    employees.update(
+      { staffNumber: req.user.staffNumber },
+      { $set: {
+        'firstName': req.body.firstName,
+        'lastName': req.body.lastName,
+        'gender': req.body.gender,
+        'contact.address.houseNumber': req.body.houseNo,
+        'contact.address.street': req.body.street,
+        'contact.address.postCode': req.body.postCode,
+        'contact.address.city': req.body.city,
+        'contact.phone': req.body.phone,
+        'contact.email': req.body.email,
+      } },
+      { upsert: true },
+      (err, numReplaced) => {
+        if (err) return console.log(err);
+        console.log(numReplaced);
+        res.redirect('/logout');
+      }
+    );
+  }
 }
 
 exports.payslips = (req, res) => {
